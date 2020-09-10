3afa1f68b9f6c6e4175350546301f89e66cda7dd
furno west <furnowest57@gmail.com>
2020-09-10 14:13:28 -0700

    done

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import SavedList from "./Movies/SavedList";
 import MovieList from "./Movies/MovieList";
 import Movie from "./Movies/Movie";
 import axios from 'axios';
+import UpdateMovie from './conponents/UpdateMovie';
 
 const App = () => {
   const [savedList, setSavedList] = useState([]);
@@ -33,8 +34,12 @@ const App = () => {
       </Route>
 
       <Route path="/movies/:id">
-        <Movie addToSavedList={addToSavedList} />
+        <Movie addToSavedList={addToSavedList} movieList={movieList} setMovieList={setMovieList}/>
       </Route>
+      <Route path='/update-movie/:id'
+      render={()=>{
+        return <UpdateMovie movies={MovieList}setMovieList={setMovieList} />
+      }}  />
     </>
   );
 };
